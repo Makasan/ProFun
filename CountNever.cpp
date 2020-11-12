28b4d1f7c3457b4251548fc806b33dbe939fc56b
Makasan <69116202+Makasan@users.noreply.github.com>
2020-11-12 21:15:48 +0700

    Add files via upload

---
+++ CountNever.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+int Check = 0, sum = 0;
+char str[201],x;
+char* p;
+
+void check() {
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (*p == 'N' or *p == 'n')
+		{
+			p++;
+			Check++;
+			if (*p == 'E' or *p == 'e') 
+			{
+				p++;
+				Check++;
+				if (*p == 'V' or *p == 'v') 
+				{
+					p++;
+					Check++;
+					if (*p == 'E' or *p == 'e') 
+					{
+						p++;
+						Check++;
+						if (*p == 'R' or *p == 'r') 
+						{
+							sum++;
+							p++;
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			p++;
+		}
+
+	}
+	printf("%d", sum);
+}
+int main() {
+
+	scanf("%[^\n]", str);
+	p = str;
+	check();
+	return 0;
+}
